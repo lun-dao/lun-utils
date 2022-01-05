61223fb00216a6038e64b5684dfeadef4f5ca1a5
hydai <z54981220@gmail.com>
2022-01-05 19:49:51 +0800

    feat: sum the donation from the same account

--- snapshot.js
+++ snapshot.js
@@ -8,16 +8,28 @@ const web3 = new Web3(RPC);
 	const targetBlock = parseInt(args[2], 10);
 	await web3.eth.net.isListening();
 
+	console.log('========== SNAPSHOT ==========');
 	console.log('Web3 is connected.');
 	console.log(`Process block ${startBlock} to ${targetBlock} on ${contractAddress}`);
 
+	console.log('\n========== CHECK TX IN BLOCKS ==========');
+	let donation = {};
 	for (let blockNumber = startBlock; blockNumber <= targetBlock; blockNumber++) {
 		let block = await web3.eth.getBlock(blockNumber, true);
 		for (let tx of block.transactions) {
 			if (tx.to == contractAddress) {
+				if (!donation.hasOwnProperty(tx.from)) {
+					donation[tx.from] = 0;
+				}
+				donation[tx.from] += parseInt(tx.value, 10);
 				console.log(`${blockNumber},${tx.hash},${tx.from},${tx.to},${tx.value}`);
 			}
 		}
+	}
+
+	console.log('\n========== SUMMARY ==========');
+	for (let account in donation) {
+		console.log(`${account},${donation[account]}`);
 	}
 
 	// close websocket connection
