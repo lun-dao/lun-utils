589fcf51adb135af025690ca626ea4e687a33f57
quesalltime <losesong@gmail.com>
2022-01-05 20:05:46 +0800

    feat: calculate amount of ETH for each sponsor

--- snapshot.js
+++ snapshot.js
@@ -13,14 +13,20 @@ const Web3 = require("web3");
 	console.log('Web3 is connected.');
 	console.log(`Process block ${startBlock} to ${targetBlock} on ${contractAddress}`);
 
+	let sponsorBalance = [];
 	for (let blockNumber = startBlock; blockNumber <= targetBlock; blockNumber++) {
 		let block = await web3.eth.getBlock(blockNumber, true);
 		for (let tx of block.transactions) {
 			if (tx.to == contractAddress) {
 				console.log(`${blockNumber},${tx.hash},${tx.from},${tx.to},${tx.value}`);
+				let balance = parseInt(tx.value);
+				sponsorBalance[tx.from] = sponsorBalance[tx.from] == undefined ? balance : sponsorBalance[tx.from]+balance;
 			}
 		}
 	}
+	console.log('sponsorBalance:',sponsorBalance);
+
+	
 
 	// close websocket connection
 	web3.currentProvider.connection.close()
